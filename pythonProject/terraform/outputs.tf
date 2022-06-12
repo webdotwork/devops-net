685499250ce62778f1dfd1915461fc5bd7d98ec7
Roland Reagan <40559167+webdotwork@users.noreply.github.com>
2022-06-12 19:37:38 +0300

    Update outputs.tf

--- pythonProject/terraform/outputs.tf
+++ pythonProject/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "internal_ip_address_dvps" {
 output "external_ip_address_dvps" {
   value = yandex_compute_instance.dvps.network_interface.0.nat_ip_address
 }
+
+output "yandex_vpc_subnet" {
+  value = resource.yandex_vpc_subnet.subnet.id
+}
